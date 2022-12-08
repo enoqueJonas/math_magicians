80f1721ff5eaf3b1e678a66bae21896808f8d28d
enoqueJonas <enoque.macanda@gmail.com>
2022-12-08 11:00:38 +0200

    FIX linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './Calculator.css';
 
 class Calculator extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
   render() {
     return (
             <div className='wrapper'>
