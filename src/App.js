53e0238b1638fff238ab8ebb7c37e10caecafc9c
enoqueJonas <enoque.macanda@gmail.com>
2022-12-08 10:53:09 +0200

    IMPROVE Calculator.js style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
+import Calculator from './components/Calculator.js';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Calculator text=""/>
   );
 }
 
