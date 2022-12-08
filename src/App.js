6f4c90a7c76a249cd33a35c04e367131dab70c03
enoqueJonas <enoque.macanda@gmail.com>
2022-12-08 11:44:26 +0200

    REFACTOR App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator.js';
 
-function App() {
-  return (
-    <Calculator text=""/>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.render = this.render.bind(this);
+  }
+
+  render() {
+    return (
+      <Calculator text={this.props.text}/>
+    );
+  }
 }
 
 export default App;
