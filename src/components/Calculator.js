21f498047074165ca45128498905f44dd0a89281
enoqueJonas <enoque.macanda@gmail.com>
2022-12-09 17:06:58 +0200

    ADD hooks to Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 import './Calculator.css';
-import calculate from '../logic/calculate.js';
 import { useState } from 'react';
+import calculate from '../logic/calculate.js';
 
 function Calculator() {
 //   constructor(props) {
@@ -12,14 +12,17 @@ function Calculator() {
 //     };
 //     this.handleClickEvent = this.handleClickEvent.bind(this);
 //   }
+  const [calcObj, setCalcObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
   const handleClickEvent = (event) => {
     const buttonName = event.target.id;
-    const obj = calculate(this.state, buttonName);
-    const { total, next, operation } = obj;
-    this.setState({
-      total,
-      next,
-      operation,
+    const obj = calculate(calcObj, buttonName);
+    // const { total, next, operation } = obj;
+    setCalcObj({
+      ...obj,
     });
   };
 
@@ -27,25 +30,25 @@ function Calculator() {
             <div className='wrapper'>
                 <div className='calculator'>
                     <ul className='row-1'>
-                        <li className='result'>{this.state.operation !== null ? this.state.next : this.state.total || 0}</li>
+                        <li className='result'>{calcObj.operation !== null ? calcObj.next : calcObj.total || 0}</li>
                     </ul>
                     <ul className='row row-2'>
                         <li className='calc-items' id='AC' onClick={handleClickEvent}>AC</li>
                         <li className='calc-items' id='+/-' onClick={handleClickEvent}>+/-</li>
                         <li className='calc-items' id='%' onClick={handleClickEvent}>%</li>
-                        <li className='calc-items calc-operation' id='÷' onClick={this.handleClickEvent}>÷</li>
+                        <li className='calc-items calc-operation' id='÷' onClick={handleClickEvent}>÷</li>
                     </ul>
                     <ul className='row row-3'>
                         <li className='calc-items' id='7' onClick={handleClickEvent}>7</li>
                         <li className='calc-items' id='8' onClick={handleClickEvent}>8</li>
-                        <li className='calc-items' id='9' onClick={this.handleClickEvent}>9</li>
-                        <li className='calc-items calc-operation' id='x' onClick={this.handleClickEvent}>x</li>
+                        <li className='calc-items' id='9' onClick={handleClickEvent}>9</li>
+                        <li className='calc-items calc-operation' id='x' onClick={handleClickEvent}>x</li>
                     </ul>
                     <ul className='row row-4'>
                         <li className='calc-items' id='4' onClick={handleClickEvent}>4</li>
                         <li className='calc-items' id='5' onClick={handleClickEvent}>5</li>
                         <li className='calc-items' id='6' onClick={handleClickEvent}>6</li>
-                        <li className='calc-items calc-operation' id='-' onClick={this.handleClickEvent}>-</li>
+                        <li className='calc-items calc-operation' id='-' onClick={handleClickEvent}>-</li>
                     </ul>
                     <ul className='row row-5'>
                         <li className='calc-items' id='1' onClick={handleClickEvent}>1</li>
@@ -59,7 +62,6 @@ function Calculator() {
                         <li className='calc-items calc-operation' id='=' onClick={handleClickEvent}>=</li>
                     </ul>
                 </div>
-                {this.props.text}
             </div>
   );
 }
