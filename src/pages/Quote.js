b87640313e01523920b4dab2b2c9a4aa04829ee6
enoqueJonas <enoque.macanda@gmail.com>
2022-12-15 20:13:02 +0200

    UPDATE page Quote.js

---
+++ src/pages/Quote.js
@@ -0,0 +1,9 @@
+const Quote = () => {
+    return(
+        <div>
+            <h2>Math is not about numbers, equations, computations or algorithms: it is about understanding. -William Paul Thurston</h2>
+        </div>
+    );
+}
+
+export default Quote;
