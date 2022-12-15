a9d43c9dbf2201333abd98a4c83fb5b4b56b5eba
enoqueJonas <enoque.macanda@gmail.com>
2022-12-16 00:12:14 +0200

    ADD Calculator.js component tests

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import Calculator from "../components/Calculator";
 
-test('Calculator renders correctly', () => {
+describe('Calculator component test', () => {
+    test('Title renders correctly', () => {
+        render(<Calculator />);
 
+        screen.getByText(`Let's do Some Math!`);
+
+        expect(screen.getByText(`Let's do Some Math!`)).toBeInTheDocument();
+
+    })
+    test('Calculator renders items correctly', () => {
+        render(<Calculator />);
+
+        screen.getByText('1');
+
+        expect(screen.getByText('1')).toBeInTheDocument();
+
+    })
 })
