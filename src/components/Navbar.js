ac44ab96dce44a0f876202e02d25ce880d367114
enoqueJonas <enoque.macanda@gmail.com>
2022-12-15 20:19:32 +0200

    ADD component Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Math Magicians</h1>
+            <ul className="navBar">
+                <li></li>
+            </ul>
+        </nav>
+    );
+}
